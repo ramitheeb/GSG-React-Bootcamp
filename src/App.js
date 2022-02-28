b549390ceca6f9e1c414c32c01fe844cccedd707
Rami Theeb <rami.theeb00@gmail.com>
2022-02-28 10:51:58 +0200

    convert posts into list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 
 import Post from "./Post"
 
+const posts = [
+    {
+        imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+    {
+        title: "a2",
+    }, {
+        title: "a1",
+        imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }, {
+        title: "a1",
+        imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }, {
+        title: "a1",
+        imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    }, {
+        title: "a1",
+        imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+]
+
+
+
 const App = () => {
 
     return (
 
         <div>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            {posts.map((post) => <Post title={post.title} imageSrc={post.imageSrc} />)}
         </div>
     );
 
