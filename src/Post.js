79a9823782281d0b9aa4520c17a0ea5a1605ade9
Rami Theeb <rami.theeb00@gmail.com>
2022-02-28 11:39:07 +0200

    Add likes to post

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,21 @@
+import React from "react"
+
+
+const state = () => {
+
+
+    return [1, 2]
+}
 
 const Post = (props) => {
+
+
+    const [likes, setLikes] = React.useState(0);
+
+    const handleClick = () => {
+        setLikes(likes + 1);
+    }
+
 
     const { title = "default title", imageSrc } = props;
     return <div>
@@ -7,6 +23,10 @@ const Post = (props) => {
         <img width={200} src={imageSrc} />
         <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
+        <div>
+            <h4>{likes} Likes</h4>
+            <button onClick={handleClick} >Like</button>
+        </div>
     </div>
 }
 
