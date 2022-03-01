d4d111d8dc2417a58d0e9aaf2116dd5386bc52a6
Rami Theeb <rami.theeb00@gmail.com>
2022-03-01 12:31:29 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,101 +1,23 @@
-
-import Post from "./Post"
 import React from "react"
 import "./styles.css"
-import Counter from "./Counter"
-
-// const initialPosts = [
-//     {
-//         imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-//     },
-//     {
-//         title: "a2",
-//     }, {
-//         title: "a1",
-//         imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-//     }, {
-//         title: "a1",
-//         imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-//     }, {
-//         title: "a1",
-//         imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-//     }, {
-//         title: "a1",
-//         imageSrc: "https://images.pexels.com/photos/5764701/pexels-photo-5764701.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-//     },
-// ]
-
-
-
-
-
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./About"
+import Posts from "./Posts";
 
 
 const App = () => {
 
-    const [title, setTitle] = React.useState("")
-    const [body, setBody] = React.useState("")
-    const [posts, setPosts] = React.useState([])
-
-
-    React.useEffect(() => {
-        console.log("mounted")
-
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-            .then(data => setPosts(data))
-            .catch(err => console.log(err))
-
-
-    }, [])
-
-
-    const [count, setCount] = React.useState(0);
-    const handleAdd = () => {
-        setCount(count + 1)
-    }
-
-
-    const handleTitleChange = (e) => {
-        setTitle(e.target.value)
-    }
-    const handleBodyChange = (e) => {
-        setBody(e.target.value)
-    }
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setPosts([
-
-            {
-                title: title,
-                body: body
-            }, ...posts
-        ])
-        setTitle("")
-        setBody("")
-    }
-
-
-
     return (
         <div>
-            <Counter count={count} handleAdd={handleAdd} />
-            <Counter count={count} handleAdd={handleAdd} />
-
-            <div>
-                <form className="form" onSubmit={handleSubmit} >
-                    <input type="text" placeholder="title" value={title} onChange={handleTitleChange} />
-                    <input type="text" placeholder="body" value={body} onChange={handleBodyChange} />
-                    <input type="submit" />
-                </form>
-            </div>
-            <div className="container">
-                {posts.map((post) => <Post title={post.title} body={post.body} />)}
-            </div>
-
-
+            <nav>
+                <Link to="/" className="navLink">Home</Link>
+                <Link to="/about" className="navLink">About</Link>
+            </nav>
+            <h1>Welcome to React Router!</h1>
+            <Routes>
+                <Route path="/" element={<Posts />} />
+                <Route path="about" element={<About />} />
+            </Routes>
         </div>
     );
 
---
+++ src/Posts.js
@@ -0,0 +1,75 @@
+import React from "react"
+import Counter from "./Counter"
+import Post from "./Post"
+const Posts = () => {
+
+    const [title, setTitle] = React.useState("")
+    const [body, setBody] = React.useState("")
+    const [posts, setPosts] = React.useState([])
+
+
+    React.useEffect(() => {
+        console.log("mounted")
+
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => setPosts(data))
+            .catch(err => console.log(err))
+
+
+    }, [])
+
+
+    const [count, setCount] = React.useState(0);
+    const handleAdd = () => {
+        setCount(count + 1)
+    }
+
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value)
+    }
+    const handleBodyChange = (e) => {
+        setBody(e.target.value)
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setPosts([
+
+            {
+                title: title,
+                body: body
+            }, ...posts
+        ])
+        setTitle("")
+        setBody("")
+    }
+
+
+
+    return (
+        <div>
+
+
+            <Counter count={count} handleAdd={handleAdd} />
+            <Counter count={count} handleAdd={handleAdd} />
+
+            <div>
+                <form className="form" onSubmit={handleSubmit} >
+                    <input type="text" placeholder="title" value={title} onChange={handleTitleChange} />
+                    <input type="text" placeholder="body" value={body} onChange={handleBodyChange} />
+                    <input type="submit" />
+                </form>
+            </div>
+            <div className="container">
+                {posts.map((post) => <Post title={post.title} body={post.body} />)}
+            </div>
+
+
+        </div>
+    );
+
+}
+export default Posts
