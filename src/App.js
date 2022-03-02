e899464688befda6f19b6ec3ef3fa486d4f6af5b
Rami Theeb <rami.theeb00@gmail.com>
2022-03-02 09:36:01 +0200

    add main

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React from "react"
-import "./styles.css"
-import { Routes, Route, Link } from "react-router-dom";
-import About from "./About"
-import Posts from "./Posts";
-
-
 const App = () => {
-
-    return (
-        <div>
-            <nav>
-                <Link to="/" className="navLink">Home</Link>
-                <Link to="/about" className="navLink">About</Link>
-            </nav>
-            <h1>Welcome to React Router!</h1>
-            <Routes>
-                <Route path="/" element={<Posts />} />
-                <Route path="about" element={<About />} />
-            </Routes>
-        </div>
-    );
-
+    return <div>
+        <h1>Hello World from App.js </h1>
+    </div>
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    , document.getElementById('root'));
+import ReactDOM from "react-dom";
+import App from "./App"
+
+
+ReactDOM.render(<App />,
+    document.getElementById("root"));
